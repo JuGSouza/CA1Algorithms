1b06cad114663423fc4617fcc21043673a1c50f5
JuGSouza <jgs.contact55@gmail.com>
2026-04-20 19:18:31 +0100

    adapting stack java
    adapting stack java from moodle to my project

--- src/StackStorage.java
+++ src/StackStorage.java
@@ -1,37 +1,35 @@
-package com.mycompany.datastructures;
-
 public class StackStorage {
-    private int[] arr;
+    private FoodItem[] arr;
     private int top;
     private int capacity;
 
     public StackStorage(int size) {
-        arr = new int[size];
+        arr = new FoodItem[size];
         capacity = size;
         top = -1;
     }
 
-    public void push(int x) {
+    public void push(FoodItem item) {
         if (top == capacity - 1) {
             System.out.println("Stack Overflow");
             return;
         }
-        arr[++top] = x;
-        System.out.println(x + " pushed to stack.");
+        arr[++top] = item;
+        System.out.println(item.getName() + " pushed to stack.");
     }
 
-    public int pop() {
+    public FoodItem pop() {
         if (top == -1) {
             System.out.println("Stack Underflow");
-            return -1;
+            return null;
         }
         return arr[top--];
     }
 
-    public int peek() {
+    public FoodItem peek() {
         if (top == -1) {
             System.out.println("Stack is empty");
-            return -1;
+            return null;
         }
         return arr[top];
     }
@@ -46,9 +44,24 @@ public class StackStorage {
             return;
         }
         System.out.print("Stack elements: ");
-        for (int i = 0; i <= top; i++) {
-            System.out.print(arr[i] + " ");
+        for (int i = top; i >= 0; i--) {
+            arr[i].display();
         }
-        System.out.println();
+    }
+    
+    public void search(String name) {
+        if (top == -1) {
+            System.out.println("Stack is empty.");
+            return;
+        }
+        for (int i = 0; i <= top; i++) {
+            if (arr[i].getName().equalsIgnoreCase(name)) {
+                System.out.println("Food item found: " + arr[i]);
+                found = true;
+            }
+        }
+        if (!found) {
+            System.out.println("Food item not found.");
+        }
     }
 }
